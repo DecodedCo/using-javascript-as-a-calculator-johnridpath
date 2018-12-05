77f76e92ed12cfac8550d31482238a553bbe207a
johnridpath <jsridpath@gmail.com>
2018-12-05 15:15:58 +0000

    Create example.js

---
+++ example.js
@@ -0,0 +1,17 @@
+/*
+
+You can use JavaScript as a calculator!
+
+*/
+
+console.log(1 + 1);
+
+/* 
+
+I am buying Christmas cards for my friends. Each card costs £2 and I have 3 friends. 
+
+Try completing the code below, so that it works out how much I will spend on Christmas cards this year.
+
+*/
+
+console.log();
